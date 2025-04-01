1e816f1e78816f2b379ae7215d0e46e02413ec3b
lora <1loraoswald1@gmail.com>
2025-04-01 19:46:45 +0500

    add main_page and other

---
+++ conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options as OptionsChrome
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser-name', action='store', default="chrome", help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default="en", help='Choose language: ru, en or other')
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    language = request.config.getoption("language")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test...")
+        options = OptionsChrome()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test...")
+        options = OptionsFirefox()
+        options.set_preference('intl.accept_languages', language)
+        browser = webdriver.Firefox(options=options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    browser.maximize_window()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
