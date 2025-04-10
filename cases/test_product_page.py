2c9db7ddcc7c1fcda1fb689e70d4b4655668853f
lora <1loraoswald1@gmail.com>
2025-04-10 14:54:25 +0500

    update test_product_page.py: add 3 new tests

--- cases/test_product_page.py
+++ cases/test_product_page.py
@@ -22,3 +22,26 @@ def test_guest_can_add_product_to_basket(browser, promo_offer):
     page.add_to_basket()
     page.check_message_about_item_in_basket()
     page.check_basket_price_message()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_disappear_success_message()
--- pages/product_page.py
+++ pages/product_page.py
@@ -10,14 +10,21 @@ class ProductPage(BasePage):
         basket.click()
         self.solve_quiz_and_get_code()
 
-
     def check_message_about_item_in_basket(self):
         item_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME_H1).text
         item_name_in_basket = self.browser.find_element(*ProductPageLocators.MESSAGE_WITH_ITEM_NAME_DIV).text
         assert item_name == item_name_in_basket, "Item name doesn't match item name in basket"
 
-
     def check_basket_price_message(self):
         item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE_P).text
         basket_total_price = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_PRICE).text
         assert item_price in basket_total_price, "Item price doesn't match basket total price"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE_DIV), \
+            "Success message is presented, but should not be"
+
+
+    def should_disappear_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE_DIV), \
+            "Success message is not disappeared, but should be"
